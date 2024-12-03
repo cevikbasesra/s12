526397bc011599bc7dd6f6105455c5cac380c648
cevikbasesra <cevikbasesra@gmail.com>
2024-12-03 19:15:17 +0300

    sayfa temasi icin usecontext kullanildi

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("darkMode");
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === "true");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("darkMode", newMode);
+      return newMode;
+    });
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
