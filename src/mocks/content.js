d54f9a7ad15b85ada0b38ee71c5285a1ff982a02
cevikbasesra <cevikbasesra@gmail.com>
2024-12-03 19:13:19 +0300

    hero'nun icindeki sozel datalar eklendi

---
+++ src/mocks/content.js
@@ -0,0 +1,31 @@
+const content = {
+  hero: {
+    title: {
+      en: "I am a Frontend Developer...",
+      tr: "Ben bir Frontend Geliştiricisiyim...",
+    },
+    description: {
+      en: "...who likes to craft solid and scalable frontend products with great user experiences.",
+      tr: "...harika kullanıcı deneyimleri ile sağlam ve ölçeklenebilir frontend ürünler üretmeyi seven biri.",
+    },
+  },
+  buttons: {
+    github: {
+      en: "GitHub",
+      tr: "GitHub",
+    },
+    linkedin: {
+      en: "LinkedIn",
+      tr: "LinkedIn",
+    },
+    darkMode: {
+      en: "DARK MODE",
+      tr: "KOYU MOD",
+    },
+    lightMode: {
+      en: "LIGHT MODE",
+      tr: "AÇIK MOD",
+    },
+  },
+};
+export default content;
